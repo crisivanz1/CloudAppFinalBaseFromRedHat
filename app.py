96b66203fad1e4a2d2d7af04856b2d7ba83f29d9
crisivanz1 <79119785+crisivanz1@users.noreply.github.com>
2022-07-20 18:36:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ def Default():
     fizzbuzz = fizz + buzz
     increment3 = 3
     increment5 = 5
-    for i in range(1,101):
-        if i % increment3 == 0 and i % increment5 == 0:
-            return fizzbuzz
-        elif i % increment3 == 0:
-            return fizz
-        elif i % increment5 == 0:
-            return buzz
-        else:
-            return i
+    i = 0
+    if i % increment3 == 0 and i % increment5 == 0:
+        return fizzbuzz
+    elif i % increment3 == 0:
+        return fizz
+    elif i % increment5 == 0:
+        return buzz
+    else:
+        return i
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
