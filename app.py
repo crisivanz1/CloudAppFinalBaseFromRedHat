1a32e221829d5e8a39c2f7168f4f39058bcffa0c
crisivanz1 <79119785+crisivanz1@users.noreply.github.com>
2022-07-20 18:29:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-
 from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "bruh"
+
+
+def Default(i):
+    fizz = "Fizz"
+    buzz = "Buzz"
+    fizzbuzz = fizz + buzz
+    increment3 = 3
+    increment5 = 5
+    if i % increment3 == 0 and i % increment5 == 0:
+        return(fizzbuzz)
+    elif i % increment3 == 0:
+        return(fizz)
+    elif i % increment5 == 0:
+        return(buzz)
+    else:
+        return(i)
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
+
+    for i in range(1,101):
+        Default(i)
 
     app.run(port=port,host='0.0.0.0')
