aac88e54b523d7bc4237c24954f9433d013489bf
crisivanz1 <79119785+crisivanz1@users.noreply.github.com>
2022-07-20 17:52:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,7 @@ app = Flask(__name__)
 
 
 def Default():
-    fizz = "Fizz"
-    buzz = "Buzz"
-    fizzbuzz = fizz + buzz
-    increment3 = 3
-    increment5 = 5
-    for i in range(1,101):
-        if i % increment3 == 0 and i % increment5 == 0:
-            print(fizzbuzz)
-        elif i % increment3 == 0:
-            print(fizz)
-        elif i % increment5 == 0:
-            print(buzz)
-        else:
-            print(i)
+    print("hello world!")
 
 
 if __name__ == '__main__':
